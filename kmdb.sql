790e885bb9e5cbfdf0d04ec652ed52be0d32306b
alisonbaird <ali.baird@kellogg.northwestern.edu>
2026-01-23 00:26:38 +0000

    post TA office hours

--- kmdb.sql
+++ kmdb.sql
@@ -95,13 +95,57 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE movies;
+DROP TABLE actors;
+DROP TABLE studios;
+DROP TABLE agents;
 
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year_released INTEGER,
+  mpaa_rating TEXT,
+  studio_id INTEGER
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  actor_first_name TEXT,
+  actor_last_name TEXT,
+  character_first_name TEXT,
+  character_last_name TEXT,
+  movie_id INTEGER,
+  agent_id INTEGER
+);
+
+CREATE TABLE studios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  studio_name TEXT
+);
+
+CREATE TABLE agents (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  agent_first_name TEXT,
+  agent_last_name TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+
+INSERT INTO movies (
+  title,
+  year_released,
+  mpaa_rating
+)
+VALUES 
+  ("Batman Begins", 2005, "PG-13"),
+  ("The Dark Knight", 2008, "PG-13");
+  
+  
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -160,44 +204,25 @@
 -- ====================
 -- Christian Bale
 
+-- CREATE TABLE students (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   first_name TEXT,
+--   last_name TEXT
+-- );
 
 
-CREATE TABLE Movies (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT,
-  year_released INTEGER,
-  mpaa_rating TEXT,
-  studio_id 
-);
 
-CREATE TABLE Actors (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  actor_first_name TEXT,
-  actor_last_name TEXT,
-  character_first_name TEXT,
-  character_last_name TEXT,
-  movie_id,
-  agent_id
-);
 
-CREATE TABLE Studios (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  studio_name
-);
 
-CREATE TABLE Agents (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  agent_first_name TEXT,
-  agent_last_name TEXT
-);
+-- INSERT INTO students (
+--   first_name,
+--   last_name,
+--   email
+-- )
+-- VALUES (
+--   "Jane",
+--   "Doe",
+--   "jane@example.com"
+-- );
 
-INSERT INTO Movies (
-  title TEXT,
-  year_released INTEGER,
-  mpaa_rating TEXT
-)
-VALUES (
-  "Batman Begins",
-  "2005",
-  "PG-13"
-);
+SELECT * FROM movies;
